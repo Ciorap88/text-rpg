aa2b8c8ae793897b9545227502b9d983f0f2f063
Ciorap88 <vlad.014916@gmail.com>
2020-04-07 14:26:00 +0300

    Updates

---
+++ precache-manifest.c3b290266bcd87dcce11e8adf54ab4ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87dadb6961e3fc1a8bf4b47eca254f2",
+    "url": "/website-react/index.html"
+  },
+  {
+    "revision": "ce52e044439871a0facf",
+    "url": "/website-react/static/css/main.4eae9c8f.chunk.css"
+  },
+  {
+    "revision": "ba74105a8b10f1275395",
+    "url": "/website-react/static/js/2.c13f4c95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website-react/static/js/2.c13f4c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce52e044439871a0facf",
+    "url": "/website-react/static/js/main.c0327265.chunk.js"
+  },
+  {
+    "revision": "de49020205491133d135",
+    "url": "/website-react/static/js/runtime-main.f2d946c4.js"
+  }
+]);
